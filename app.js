10b88e912f6a4662eb65de8b0c2bf1099c57ac64
Orlando <isc.orangelc@gmail.com>
2018-04-03 00:49:54 -0500

    CRUD Usuario

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 //Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 //Initialize
 var app = express();
+
+
+//Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',( err, res )=>{
 
     if( err ) throw err;
@@ -12,15 +20,14 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB',( err, res )=
 
 });
 
+//Import Routes
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+
+
 //Routes
-app.get('/', (req, res) =>{
-
-res.status(200).json({
-    ok: true,
-    mensaje: 'Petición realizada correctamente'
-});
-
-});
+app.use('/usuario',usuarioRoutes);
+app.use('/',appRoutes);
 
 //Listen requests
 app.listen(3000,() => {
